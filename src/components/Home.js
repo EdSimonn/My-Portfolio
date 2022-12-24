c3415992d1e9b950070db57a020f0dab23512021
EdSimonn <dami25edusi@gmail.com>
2022-12-24 15:24:32 -0800

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,34 +6,27 @@ import { staggerContainer, fadeIn, textVariant } from "../utils/motion";
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#100720]">
-      <div
-        className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full "
-      >
-        <motion.p
-          variants={fadeIn("up", "tween", 0.1, 1)}
-          className="custom text-pink-600 mb-6"
-        >
-          Hola, my name is
-        </motion.p>
-        <motion.h1 variants={textVariant(0.2)} className="text-white text-4xl sm:text-7xl font-bold text-gray mb-6">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+        <p className="custom text-pink-600 mb-6">Hola, my name is</p>
+        <h1 className="text-white text-4xl sm:text-7xl font-bold text-gray mb-6">
           Simon Edusi.
-        </motion.h1>
-        <motion.h1 variants={textVariant(0.3)} className="text-white text-4xl sm:text-7xl font-bold text-gray mb-6">
+        </h1>
+        <h1 className="text-white text-4xl sm:text-7xl font-bold text-gray mb-6">
           I'm a Web Developer.
-        </motion.h1>
-        <motion.p variants={fadeIn('up', 'tween', 0.4, 1)} className="text-gray-400 py-4 max-w-[700px] mb-6">
+        </h1>
+        <p className="text-gray-400 py-4 max-w-[700px] mb-6">
           Specializing mostly in frontend development. I give life to beautiful
           UI/UX design through codes, using the right front-end web technologies
           and techniques to create fast and responsive websites.
-        </motion.p>
-        <motion.div variants={fadeIn('up', 'tween', 0.5, 1)}>
+        </p>
+        <div>
           <button className="text-pink-600 border-pink-600 border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-black hover:text-white duration-500">
             View Resume
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
-        </motion.div>
+        </div>
       </div>
     </div>
   );
